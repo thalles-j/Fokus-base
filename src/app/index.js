916769606f8e36794e7156f54f442354fd010715
Thalles José <thallesjosesv@gmail.com>
2024-10-09 12:32:33 -0300

    alterar fundo

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-const html = document.querySelector('html');
+import { alterarContexto } from './alterarContexto.js';
+
+
+const btnAll = document.querySelectorAll('.app__card-button');
+
+let ultimoClick = null;
+
+btnAll.forEach(btn => btn.addEventListener('click', (e) => {
+    // Pega o valor do atributo data-contexto do botão clicado
+    const btnContexto = btn.getAttribute('data-contexto');
+    
+    if(ultimoClick){
+        ultimoClick.classList.remove('active');
+    }
+    btn.classList.add('active');
+        
+    ultimoClick = btn;
+    alterarContexto(btnContexto)
+   
+    
+}));
