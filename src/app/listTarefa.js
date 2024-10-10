01cf1797e241ef65425adfd69a62a27b3a1ed2fb
thalles-j <thallesjosesv@gmail.com>
2024-10-10 19:12:20 -0300

    list taref

---
+++ src/app/listTarefa.js
@@ -0,0 +1,20 @@
+const btnAddTask = document.querySelector('.app__button--add-task');
+const formAddTak = document.querySelector('.app__form-add-task');
+const textAreaForm = document.querySelector('.app__form-textarea');
+
+const task = []
+
+btnAddTask.addEventListener('click', () => {
+    formAddTak.classList.toggle('hidden-task');
+
+});
+
+formAddTak.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskObjet = {
+        descricao: textAreaForm.value,
+    }
+    task.push(taskObjet);
+    localStorage.setItem('task', taskObjet)
+
+})
